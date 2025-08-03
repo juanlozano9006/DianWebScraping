1317bebe6d93cdd1b23a05609955b44c8f9deb36
juanlozano9006 <juan_klozano@hotmail.com>
2025-08-03 16:37:54 -0500

    se crea archivo de requerimientos

--- Dian/logicaSelenium/webScrapingDian copy.py
+++ Dian/logicaSelenium/webScrapingDian copy.py
@@ -24,7 +24,7 @@ import zipfile
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
-import boto3
+
 from botocore.exceptions import BotoCoreError, ClientError
 
 
